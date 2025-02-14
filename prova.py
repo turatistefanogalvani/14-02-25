6ae33f446c96e5533e82fabab21451159d0cc043
turatistefanogalvani 123 <turati.stefano@iisgalvanimi.edu.it>
2025-02-14 12:00:57 +0000

    terzo commit

--- prova.py
+++ prova.py
@@ -1,2 +1,20 @@
-print(0)
-print("secondo commit")
+inventario = {
+    1 : {"nome": "Il PC di ...", "quantità": 1, "prezzo": 3999},
+    2 : {"nome": "Nintendo Switch 2", "quantità": 10, "prezzo": 449},
+    3 : {"nome": "GTA VI", "quantità": 3, "prezzo": 100}
+}
+def aggiunta():
+    chiave = len(inventario) + 1
+    nome = input("inserisci un nome: ")
+    quantità = int(input("inserisci la quantità: "))
+    prezzo = int(input("inserisci il prezzo: "))
+    inventario[chiave] = {"nome" : nome , "quantità" : quantità , "prezzo" : prezzo}
+    print(inventario)
+print(aggiunta())
+
+def modifica_quantità():
+    if chiave in studente.keys():
+        studente["quantità"] = input("inserisci nuovo valore")
+        print(studente)
+    else:
+        print("la chiave non esiste")
